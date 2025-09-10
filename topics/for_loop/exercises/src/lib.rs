59576c8f7c05323a9ab9e1d022e3cb051fcf8d44
Your Name <suhasmashety@gmail.com>
2025-09-10 14:31:27 +0000

    completed for_loop

--- topics/for_loop/exercises/src/lib.rs
+++ topics/for_loop/exercises/src/lib.rs
@@ -1,7 +1,15 @@
 pub fn sum(nums: Vec<i32>) -> i32 {
-    todo!();
+    let mut total = 0;
+    for num in nums {
+        total = total + num;
+    }
+    total
 }
 
 pub fn fill(i: u32, n: usize) -> Vec<u32> {
-    todo!();
+    let mut vec = Vec::new();
+    for _ in 0..n {
+        vec.push(i);
+    }
+    vec
 }
