d7c2fe156340e99b75829164b993ea73f266f315
Your Name <suhasmashety@gmail.com>
2025-10-01 15:15:40 +0000

    completed generic trait

--- topics/generic_trait/exercises/src/lib.rs
+++ topics/generic_trait/exercises/src/lib.rs
@@ -14,6 +14,25 @@ pub struct VecIter<T> {
 
 impl<T> Iterator<T> for TupleIter<T> {
     fn next(&mut self) -> Option<&T> {
-        todo!();
+        let result = match self.next {
+            0 => Some(&self.tuple.0),
+            1 => Some(&self.tuple.1),
+            2 => Some(&self.tuple.2),
+            _ => None,
+        }
+        self.next += 1;
+        result
+    }
+}
+
+impl<T> Iterator<T> for VecIter<T> {
+    fn next(&mut self) -> Option<&T> {
+        if self.next < self.vec.len() {
+            let result = Some(&self.vec[self.next]);
+            self.next += 1;
+            result
+        } else {
+            None
+        }
     }
 }
